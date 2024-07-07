151cc572556c54084b71828aee80ac6f3d8a3b95
burak-senel <buurak.senel@gmail.com>
2024-07-08 01:31:00 +0300

    all tasks done!

---
+++ src/main/java/com/workintech/s18d1/entity/Burger.java
@@ -0,0 +1,28 @@
+package com.workintech.s18d1.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Getter
+@Table(name="burger",schema = "fsweb")
+public class Burger {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name="name")
+    private String name;
+    @Column(name="price")
+    private Double price;
+    @Column(name="is_vegan")
+    private Boolean isVegan;
+    @Column(name="bread_type")
+    @Enumerated(EnumType.STRING)
+    private BreadType breadType;
+    @Column(name="contents")
+    private String contents;
+}
