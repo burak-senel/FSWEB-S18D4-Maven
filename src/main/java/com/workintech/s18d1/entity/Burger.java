dea10b66aba5a54377fff5276c4cf633ef5733c3
burak-senel <buurak.senel@gmail.com>
2024-07-08 16:16:02 +0300

    all tasks done!

--- src/main/java/com/workintech/s18d1/entity/Burger.java
+++ src/main/java/com/workintech/s18d1/entity/Burger.java
@@ -8,21 +8,26 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @Entity
-@Getter
 @Table(name="burger",schema = "fsweb")
 public class Burger {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     @Column(name="name")
     private String name;
+
     @Column(name="price")
     private Double price;
+
     @Column(name="is_vegan")
     private Boolean isVegan;
+
     @Column(name="bread_type")
     @Enumerated(EnumType.STRING)
     private BreadType breadType;
+
     @Column(name="contents")
     private String contents;
 }
